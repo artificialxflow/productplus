e39e33265ca05a5ec8ba7dceed6ab6882abb4f1e
zedparman <ze.dparman@gmail.com>
2025-08-18 09:34:32 -0700

    Create page.tsx

---
+++ app/admin/users/page.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+import UserManagement from '@/components/UserManagement'
+import Header from '@/components/Header'
+
+export default function AdminUsersPage() {
+  return (
+    <>
+      <Header />
+      <UserManagement />
+    </>
+  )
+}
