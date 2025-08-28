3d4c7a998cc675713b78bca3326df10bae393356
zedparman <ze.dparman@gmail.com>
2025-08-28 07:24:49 -0700

    Update route.ts

--- app/api/products/[id]/route.ts
+++ app/api/products/[id]/route.ts
@@ -1,7 +1,5 @@
 import { type NextRequest, NextResponse } from "next/server"
-import { PrismaClient } from "@prisma/client";
-
-const prisma = new PrismaClient()
+import { prisma } from "../../../../lib/prisma";
 
 // دریافت محصول خاص
 export async function GET(
