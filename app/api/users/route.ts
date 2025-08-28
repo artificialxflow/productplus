849573fd03ed3070d18063983762696712fcc377
zedparman <ze.dparman@gmail.com>
2025-08-28 07:22:36 -0700

    Update route.ts

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,7 +1,5 @@
 import { NextRequest, NextResponse } from "next/server"
-import { PrismaClient } from "@prisma/client";
-
-const prisma = new PrismaClient()
+import { prisma } from "../../../lib/prisma";
 
 // دریافت لیست کاربران (فقط برای مدیران)
 export async function GET(request: NextRequest) {
